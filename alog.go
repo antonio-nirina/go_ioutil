523657524cae5fbf5e6c83c656e321b809b141f3
Antonio <antonio.jacques@xipe.com>
2020-02-07 15:18:31 +0100

    test

--- alog.go
+++ alog.go
@@ -1,25 +1,24 @@
 package main
 
 import (
-
-	log "github.com/sirupsen/logrus"
+  log "github.com/sirupsen/logrus"
 )
 
-
 func InfoA(op string, args ...interface{}) {
-	log.SetFormatter(&log.JSONFormatter{})
-	log.WithFields(log.Fields{
-    "op": op,
-    "args":   args,
+  log.SetFormatter(&log.JSONFormatter{})
+  log.WithFields(log.Fields{
+    "op":   op,
+    "args": args,
   }).Info(args)
 
 }
 
+//
 // ErrorOp logs an error for given
 // logical operation.
 func ErrorOp(op string, err error) {
-	log.WithFields(log.Fields{
-    "op":    op,
+  log.WithFields(log.Fields{
+    "op":  op,
     "err": err,
   }).Error(err.Error())
 }
@@ -27,8 +26,8 @@ func ErrorOp(op string, err error) {
 // ErrorfOp logs an error message for given
 // logical operation and format.
 func DebugOp(op, format string, args ...interface{}) {
-	log.SetFormatter(&log.JSONFormatter{})
-	log.WithFields(log.Fields{
+  log.SetFormatter(&log.JSONFormatter{})
+  log.WithFields(log.Fields{
     "op":    op,
     "debug": args,
   }).Debug(args)
@@ -36,10 +35,9 @@ func DebugOp(op, format string, args ...interface{}) {
 
 // FatalOp logs an error for given
 // logical operation and exit 1.
-func  FatalOp(op string, err error) {
-	log.WithFields(log.Fields{
-    "op": op,
+func FatalOp(op string, err error) {
+  log.WithFields(log.Fields{
+    "op":  op,
     "err": err,
   }).Fatal(err.Error())
 }
-
